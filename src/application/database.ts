faa132756afac44a461d4a2d2abdabb1a8a97a79
Muhamad Yusuf Al Farizzi <muhammadyusufalfarizzi@gmail.com>
2026-07-20 17:38:10 +0700

    setup prisma client, logging, winston

--- src/application/database.ts
+++ src/application/database.ts
@@ -1,7 +1,57 @@
 import "dotenv/config";
 import { PrismaClient } from "../generated/prisma/client.js";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import { logger } from "./logging.js";
 
-const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
+const databaseUrl = process.env.DATABASE_URL;
 
-export const prismaClient = new PrismaClient({ adapter });
+if (!databaseUrl) {
+    throw new Error("DATABASE_URL is not defined");
+}
+
+const adapter = new PrismaMariaDb(databaseUrl);
+
+export const prismaClient = new PrismaClient({ 
+    adapter,
+    log: [
+    {
+      emit: "event",
+      level: "query",
+    },
+    {
+      emit: "event",
+      level: "error",
+    },
+    {
+      emit: "event",
+      level: "info",
+    },
+    {
+      emit: "event",
+      level: "warn",
+    },
+  ],
+});
+
+prismaClient.$on("error", (e) => {
+    logger.error({
+        message: e.message,
+        target: e.target
+    });
+});
+
+prismaClient.$on("warn", (e) => {
+    logger.warn(e);
+})
+
+prismaClient.$on("info", (e) => {
+    logger.info(e);
+})
+
+prismaClient.$on("query", (e) => {
+    logger.debug({
+        query: e.query,
+        params: e.params,
+        duration: `${e.duration} ms`
+    });
+});
