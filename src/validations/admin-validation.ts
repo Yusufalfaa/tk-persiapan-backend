c66b2ae0d7e466d82532a32138210c235e1ea668
Muhamad Yusuf Al Farizzi <muhammadyusufalfarizzi@gmail.com>
2026-08-13 12:59:34 +0700

    add validation

--- src/validations/admin-validation.ts
+++ src/validations/admin-validation.ts
@@ -5,7 +5,7 @@ export class AdminValidation {
 
     static readonly CREATE : ZodType<AdminRequest> = z.object ({
         username: z.string().min(3).max(50),
-        password: z.string().min(6).max(255),
+        password: z.string().min(8).max(255),
         name: z.string().min(3).max(100),
     })
 
